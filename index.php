f1223d5998c63c2d80025aff27011f06b15cd505
Flinty916 <38919621+Flinty916@users.noreply.github.com>
2020-10-31 17:57:53 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 include('App/app.includes.php');
 
 $db = new \mystats\Database();
-
 $player = new \mystats\Player(76561198044219921);
-
-$item = new \mystats\Item('14_pier_14_nav_pier_c_l');
 ?>
 
 <!<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
@@ -17,14 +14,6 @@ $item = new \mystats\Item('14_pier_14_nav_pier_c_l');
 <body>
     <pre>
         <?php
-//        $split = str_split(json_decode($player->getCivStats()));
-//        $array = array();
-//            foreach($split as $s) {
-//                if($s !== '"' && $s !== "[" && $s !== "]") {
-//                    $array[] = $s;
-//                }
-//            }
-//            print_r(explode(',', implode($array)));
         print_r($player->getInv());
         ?>
     </pre>
